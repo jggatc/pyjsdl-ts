11d567a73633737b2870f08336af34a54b6e1279
James Garnon <jg.gatc@gmail.com>
2022-03-22 11:34:22 -0400

    refactor transform to optimize

--- pyjsdl/transform.py
+++ pyjsdl/transform.py
@@ -3,6 +3,7 @@
 
 from math import pi as _pi, sin as _sin, cos as _cos, ceil as _ceil
 from pyjsdl.pyjsobj import fabs as _fabs
+from pyjsdl.pylib import int
 from pyjsdl.surface import Surface
 
 __docformat__ = 'restructuredtext'
@@ -20,12 +21,12 @@ def rotate(surface, angle):
     if not angle:
         return surface.copy()
     theta = angle * _deg_rad
-    width_i = surface.get_width()
-    height_i = surface.get_height()
-    cos_theta = _fabs( _cos(theta) )
-    sin_theta = _fabs( _sin(theta) )
-    width_f = int( (width_i * cos_theta) + (height_i * sin_theta) )
-    height_f = int( (width_i * sin_theta) + (height_i * cos_theta) )
+    width_i = surface.width
+    height_i = surface.height
+    cos_theta = _fabs(_cos(theta))
+    sin_theta = _fabs(_sin(theta))
+    width_f = int((width_i * cos_theta) + (height_i * sin_theta))
+    height_f = int((width_i * sin_theta) + (height_i * cos_theta))
     surf = Surface((width_f, height_f))
     surf.saveContext()
     surf.translate(width_f/2.0, height_f/2.0)
@@ -40,18 +41,18 @@ def rotozoom(surface, angle, size):
     Return Surface rotated and resized by the given angle and size.
     """
     if not angle:
-        width = int(surface.get_width() * size)
-        height = int(surface.get_height() * size)
+        width = int(surface.width * size)
+        height = int(surface.height * size)
         return scale(surface, (width, height))
     theta = angle * _deg_rad
-    width_i = int(surface.get_width() * size)
-    height_i = int(surface.get_height() * size)
-    cos_theta = _fabs( _cos(theta) )
-    sin_theta = _fabs( _sin(theta) )
-    width_f = int( _ceil((width_i * cos_theta) + (height_i * sin_theta)) )
+    width_i = int(surface.width * size)
+    height_i = int(surface.height * size)
+    cos_theta = _fabs(_cos(theta))
+    sin_theta = _fabs(_sin(theta))
+    width_f = int(_ceil((width_i * cos_theta) + (height_i * sin_theta)))
     if width_f % 2:
         width_f += 1
-    height_f = int( _ceil((width_i * sin_theta) + (height_i * cos_theta)) )
+    height_f = int(_ceil((width_i * sin_theta) + (height_i * cos_theta)))
     if height_f % 2:
         height_f += 1
     surf = Surface((width_f, height_f))
@@ -59,7 +60,7 @@ def rotozoom(surface, angle, size):
     surf.translate(width_f/2.0, height_f/2.0)
     surf.rotate(-theta)
     surf.drawImage(surface.canvas,
-                   0, 0, surface.get_width(), surface.get_height(),
+                   0, 0, surface.width, surface.height,
                    -width_i/2, -height_i/2, width_i, height_i)
     surf.restoreContext()
     return surf
@@ -75,7 +76,7 @@ def scale(surface, size, dest=None):
     else:
         surf = dest
     surf.drawImage(surface.canvas,
-                   0, 0, surface.get_width(), surface.get_height(),
+                   0, 0, surface.width, surface.height,
                    0, 0, size[0], size[1])
     return surf
 
@@ -93,8 +94,7 @@ def scale2x(surface, dest=None):
     Return Surface resized to twice its size.
     An optional destination surface can be provided.
     """
-    return scale(surface,
-                 (surface.get_width()*2, surface.get_height()*2), dest)
+    return scale(surface, (surface.width*2, surface.height*2), dest)
 
 
 # __pragma__ ('kwargs')
@@ -103,16 +103,16 @@ def flip(surface, xbool=True, ybool=False):
     """
     Return Surface that is flipped horizontally, vertically, or both.
     """
-    surf = Surface((surface.get_width(), surface.get_height()))
+    surf = Surface((surface.width, surface.height))
     surf.saveContext()
     if xbool and ybool:
-        surf.translate(surface.get_width(), surface.get_height())
+        surf.translate(surface.width, surface.height)
         surf.scale(-1, -1)
     elif xbool:
-        surf.translate(surface.get_width(), 0)
+        surf.translate(surface.width, 0)
         surf.scale(-1, 1)
     elif ybool:
-        surf.translate(0, surface.get_height())
+        surf.translate(0, surface.height)
         surf.scale(1, -1)
     surf.drawImage(surface.canvas, 0, 0)
     surf.restoreContext()
